54acc388779ff13094d051f4246ea4982314dd88
ZoesDev <6062364+ZoesDev@users.noreply.github.com>
2022-05-05 12:16:21 -0700

    updated first user-datascript to include most of the docker stuff. added action to create documentation for each

--- user_data.sh
+++ user_data.sh
@@ -6,14 +6,35 @@ sudo apt update
 sudo apt upgrade -y #updating system image to make sure everything is updated
 
 
-#install nodejs
-echo "installing Nodejs"
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-sudo apt-get install -y nodejs
+#setup docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+
+# install packages that will be needed
+sudo apt-get install -y unzip jq nginx certbot python3-certbot-nginx docker-ce docker-compose
 
 #install sogebot
 echo "install sogebot"
 
+#download zip
+cd /tmp
+LOCATION=$(curl -s https://api.github.com/repos/sogebot/sogeBot-docker/releases/latest \
+| grep "zipball_url" \
+| awk '{ print $2 }' \
+| sed 's/,$//'       \
+| sed 's/"//g' )     \
+; curl -L -o sogebot.zip $LOCATION
+
+#unzip file
+
+unzip /tmp/sogebot.zip –d /home/ubuntu/sogebot
+
+
+docker-compose -f /home/ubuntu/sogebot/docker-compose.yml
 
 #check if there is a backup file in the s3 db if not skip. will be skipped on frist run as there wont be any backup around
 
